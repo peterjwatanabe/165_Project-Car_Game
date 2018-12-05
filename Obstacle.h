026e02c368f29783c71b5486a0061b6ee4c96b9f
Peter Watanabe <pjwatanabe99@gmail.com>
2018-12-04 17:21:09 -0800

    Added Obstacle and randomGen

---
+++ Obstacle.h
@@ -0,0 +1,23 @@
+#ifndef Obstacle_h
+#define Obstacle_h
+#include "TexRect.h"
+#include "randomGen.h"
+
+class Obstacle : public TexRect{
+protected:
+
+public:
+	Obstacle() {};
+
+	void Spawn() {
+
+	}
+
+	void Despawn() {
+
+	}
+
+}
+
+
+#endif
---
+++ randomGen.h
@@ -0,0 +1,12 @@
+#ifndef randomGen_h
+#define randomGen_h
+#include <cstdlib>
+#include <time.h>
+
+int getRandom() {
+	srand(time(NULL));
+	int randNum = (rand() % 5) + 1;	// generates a number 1-5
+	return randNum;
+}
+
+#endif
