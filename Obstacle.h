11df4bb53dc551eb529ee8b4a26a69e410637c09
Peter Watanabe <pjwatanabe99@gmail.com>
2018-12-04 17:47:44 -0800

    More additons to Obstacle.h

--- Obstacle.h
+++ Obstacle.h
@@ -5,16 +5,39 @@
 
 class Obstacle : public TexRect{
 protected:
-
+	int lane;
 public:
-	Obstacle() {};
+	Obstacle(const char* map_filename) : TexRect(map_filename, x, y, w, h) {
+		lane = getRandom();
+	};
+
+	void newLane() {
+		lane = getRandom();
+	}
 
 	void Spawn() {
+		if (lane == 1) {
 
+		};
+		if (lane == 2) {};
+		if (lane == 3) {};
+		if (lane == 4) {};
+		if (lane == 5) {};
 	}
 
 	void Despawn() {
+		newLane();
+	}
 
+	void movement() {
+
+	}
+
+	void getHit(int positonX, int positonY) {
+		if (contains(positionX, positonY)) {
+			// animate car explostion
+			// game over screen
+		}
 	}
 
 }
