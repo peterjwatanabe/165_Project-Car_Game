5fb37b39c9db2cdecc2878aad22049832164d70d
Peter Watanabe <pjwatanabe99@gmail.com>
2018-12-10 09:49:03 -0800

    Base was updated by professor.

--- App.cpp
+++ App.cpp
@@ -2,9 +2,9 @@
 #include "App.h"
 
 App::App(int argc, char** argv): GlutApp(argc, argv){
-    explosion = new AnimatedRect("../fireball.bmp", 6, 6, 100, -0.5, 0.5, 0.5, 0.5);
+    explosion = new AnimatedRect("fireball.bmp", 6, 6, 100, -0.5, 0.5, 0.5, 0.5);
     
-    fastExplosion = new AnimatedRect("../fireball.bmp", 6, 6, 10, 0.5, 0.5, 0.5, 0.5);
+    fastExplosion = new AnimatedRect("fireball.bmp", 6, 6, 10, 0.5, 0.5, 0.5, 0.5);
 }
 
 void App::draw() {
@@ -22,6 +22,7 @@ void App::keyDown(unsigned char key, float x, float y){
         explosion->playOnce();
     }
 }
+
 
 App::~App(){
     std::cout << "Exiting..." << std::endl;
